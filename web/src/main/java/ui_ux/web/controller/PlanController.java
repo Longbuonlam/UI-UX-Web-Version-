3b74fa2565b264a5efd30de48e0b75b6b8b35a07
Vo Son Long <97535432+Longbuonlam@users.noreply.github.com>
2024-05-30 21:20:00 +0700

    Add Creat Plan feature

---
+++ web/src/main/java/ui_ux/web/controller/PlanController.java
@@ -0,0 +1,12 @@
+package ui_ux.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PlanController {
+    @GetMapping("/KPIEdu/Plan/Create")
+    public String CreatePlan(){
+        return "CreatePlan";
+    }
+}
