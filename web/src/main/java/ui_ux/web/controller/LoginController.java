91b612f2f8a4f64deaa856e7074858ff4ecc30e1
Vo Son Long <97535432+Longbuonlam@users.noreply.github.com>
2024-05-30 12:33:52 +0700

    Add Login, SignUp, and Home Page

--- web/src/main/java/ui_ux/web/controller/LoginController.java
+++ web/src/main/java/ui_ux/web/controller/LoginController.java
@@ -4,10 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class ProjectController {
-    @GetMapping("/KPIEdu/Home")
-    public String Dashboard(){
-        return "Dashboard";
+public class LoginController {
+    @GetMapping("/KPIEdu/Login")
+    public String Login(){
+        return "Login";
     }
-
 }
--- web/src/main/java/ui_ux/web/controller/SignUpController.java
+++ web/src/main/java/ui_ux/web/controller/SignUpController.java
@@ -4,10 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class ProjectController {
-    @GetMapping("/KPIEdu/Home")
-    public String Dashboard(){
-        return "Dashboard";
+public class SignUpController {
+    @GetMapping("/KPIEdu/SignUp")
+    public String Signup(){
+        return "Signup";
     }
-
 }
