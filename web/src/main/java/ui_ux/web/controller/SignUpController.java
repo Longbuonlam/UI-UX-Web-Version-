3615191da2a4d0deddd9777e577cedf771af18f3
Vo Son Long <97535432+Longbuonlam@users.noreply.github.com>
2024-05-30 18:23:54 +0700

    Fix Signup Page and add Activity History view

--- web/src/main/java/ui_ux/web/controller/SignUpController.java
+++ web/src/main/java/ui_ux/web/controller/SignUpController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class SignUpController {
     @GetMapping("/KPIEdu/SignUp")
     public String Signup(){
-        return "Signup";
+        return "FixSignup";
     }
 }
